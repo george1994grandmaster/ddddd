699167bf3b62486f03f749c87784336ab0243545
giorgi <gkiladze1994@gmail.com>
2023-12-10 00:37:59 +0300

    ımg

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-
+import Image from 'next/image'
+import mercedes from '../../img/mercedes.png'
 
 interface DataItem {
   id: number;
@@ -36,7 +37,18 @@ export default function About() {
         <div key={index}>
           <span>ID: {item.id}</span>
           <span>Title: {item.title}</span>
-          
+          <Image
+            src='https://finder.porsche.com.tr/Assets/img/homeModel__911.png'
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
+          <Image
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7M5v7KGkDwEMzbFcCTggGoi_xvAaSOTfMA_FQKgtTM771mlTXx1OyQAFjGkfJKx0S7DU&usqp=CAU'
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
         </div>
       ))}
     </div>
